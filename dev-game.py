f2d2554f7adc5fd322f352643c0a7917ef07a96a
dannguyen07 <nguyenthanhdat07@outlook.com>
2024-05-11 22:00:26 +0700

    game init

--- dev-game.py
+++ dev-game.py
@@ -1,1 +1,17 @@
-print("hello world")
+import pygame
+pygame.init()
+
+screen = pygame.display.set_mode((400 , 650))
+
+running = True 
+while running: 
+    screen.blit((0 , 0 , 0))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+
+    pygame.display.update()
+
+pygame.quit()
