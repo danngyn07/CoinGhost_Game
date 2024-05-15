4939be8dc8811c16450d6b899d3f1bc26937d8b1
Tung Dinh Quang <117490120+qtungnui07@users.noreply.github.com>
2024-05-15 07:49:40 +0700

    Update main3.py

--- main3.py
+++ main3.py
@@ -24,6 +24,25 @@ def spawn_coin():
     coin_rect = coin_image.get_rect(center=(random.randint(0, 700), random.randint(0, 400)))
     return coin_rect
 
+def drawWindowGame():
+    global move_Count
+    if move_Count + 1 >= 12:
+        move_Count = 0
+    if right != True and left != True and down != True and up != True:
+        screen.blit(character , character_rect)
+    if right:
+        screen.blit(move_right[move_Count//4] , character_rect)
+        move_Count += 1
+    if left:
+        screen.blit(move_left[move_Count//4] , character_rect)
+        move_Count += 1
+    if up:
+        screen.blit(move_up[move_Count//4] , character_rect)
+        move_Count += 1
+    if down:
+        screen.blit(move_down[move_Count//4] , character_rect)
+        move_Count += 1
+
 def reset_game():
     global character_rect, ghosts, coins, point, ghost_spawn_timer, coin_spawn_timer, game_over, high_score
     character_rect = character.get_rect(center=(350, 200))
@@ -40,8 +59,8 @@ def reset_game():
 coin_image = pygame.image.load('environment_11.png')
 bg = pygame.image.load('ground_04.png')
 bg_width, bg_height = bg.get_size()
+character = pygame.image.load('move_down.png')
 bg = pygame.transform.scale(bg, (bg_width * 2, bg_height * 2))  # Phóng to nền
-character = pygame.image.load('playerFace_dark.png')
 ghost_image = pygame.image.load('ghost.png')
 game_over_image = pygame.image.load('gameover.png')
 
@@ -55,6 +74,14 @@ font = pygame.font.Font('freesansbold.ttf', 32)
 character_x = 350
 character_y = 200
 character_rect = character.get_rect(center=(character_x, character_y))
+
+# Character Movement
+move_down = [pygame.image.load('move_down.png') , pygame.image.load('move_down1.png') ,pygame.image.load('move_down2.png')]
+move_up = [pygame.image.load('move_up.png') , pygame.image.load('move_up1.png') , pygame.image.load('move_up2.png')]
+move_right = [pygame.image.load('move_r.png') , pygame.image.load('move_r1.png') , pygame.image.load('move_r2.png')]
+move_left = [pygame.image.load('move_l.png') ,pygame.image.load('move_l1.png') , pygame.image.load('move_l2.png')]
+right , left  , up , down = False , False , False , False
+move_Count = 0
 
 # Ghost setup
 ghosts = []
@@ -88,20 +115,36 @@ while running:
 
     if not game_over:
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_RIGHT] and character_rect.centerx <= bg.get_width() - 25:
+        if keys[pygame.K_RIGHT] and character_rect.centerx <= 675:
             character_rect.centerx += character_change_movement_speed
-        if keys[pygame.K_LEFT] and character_rect.centerx >= 25:
+            left , up , down = False , False , False   
+            right = True
+        
+        if keys[pygame.K_LEFT] and character_rect.centerx > 20:
             character_rect.centerx -= character_change_movement_speed
-        if keys[pygame.K_DOWN] and character_rect.centery <= bg.get_height() - 25:
+            right , up , down = False,False ,False 
+            left = True
+            
+        if keys[pygame.K_DOWN] and character_rect.centery <= 375:
             character_rect.centery += character_change_movement_speed
-        if keys[pygame.K_UP] and character_rect.centery >= 25:
+            right , left , up = False,False ,False 
+            down = True
+            
+        if keys[pygame.K_UP] and character_rect.centery >= 20:
             character_rect.centery -= character_change_movement_speed
-
+            right , left , down = False , False , False 
+            up = True
+        
+    
+        drawWindowGame()
+        right , left  , up , down = False , False , False , False  
+         # Spawn ghosts
         ghost_spawn_timer += clock.get_time()
         if ghost_spawn_timer >= ghost_spawn_interval and len(ghosts) < 4:
             ghosts.append(spawn_ghost())
             ghost_spawn_timer = 0
 
+        # Spawn coins
         coin_spawn_timer += clock.get_time()
         if coin_spawn_timer >= coin_spawn_interval and len(coins) <= 5:
             coins.append(spawn_coin())
@@ -110,12 +153,15 @@ while running:
         draw_background()
         screen.blit(character, character_rect)
 
+        
+        #Ghost logic
         for ghost_rect in ghosts:
             screen.blit(ghost_image, ghost_rect)
             ghost_follow(ghost_rect)
             if character_rect.colliderect(ghost_rect):
                 game_over = True
 
+        #Coin logic
         for coin_rect in coins:
             screen.blit(coin_image, coin_rect)
             if character_rect.colliderect(coin_rect):
